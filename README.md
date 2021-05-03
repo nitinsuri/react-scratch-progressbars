26770e5b27aad651014b1c2ad1b984344b3b68f8
Nitin Suri <Nitin.Suri@transport.nsw.gov.au>
2021-05-03 10:36:43 +1000

    UI update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# react-scratch-base
-react-scratch-base
+# TechM Progressbars Demo App
+## Setting up the code
+1. Clone the code
+2. run 
+## Scripts available:
+1. Start: 
--- src/js/components/ProgressBarSelector/ProgressBarSelector.js
+++ src/js/components/ProgressBarSelector/ProgressBarSelector.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ProgressBarSelector(props) {
 	return <select id="progressbar-selector" onChange={props.onChange} data-testid="progressbar-selector">
 		{
-			props.options.map((i) => <option key={i} value={i}>{"Progress bar " + (i + 1)}</option>)
+			props.options.map((option, i) => <option key={option} value={i}>{"Progress bar " + (i + 1)}</option>)
 		}
 	</select>;
 };
